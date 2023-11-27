c67421e71a38f3bf7782a4d8e54b4fc7e5a29b17
3041TAN-03 <3041TAN-03@boronkay.local>
2023-11-27 11:18:22 +0100

    function getmakers elkeszitese

--- csv-tools.php
+++ csv-tools.php
@@ -14,4 +14,38 @@ function getCsvData($fileName, $withHeader = true) {
         
     return $lines;
 }
+
+function getmakers($csvData){
+    $header = $csvData[0];
+    $keyMaker = array_search('make', $header);
+    //$keyModel = array_search('model',$header);
+    //$result=[];
+    $maker = '';
+    $makers=[]; 
+    //$model = '';
+    $isHeader=true;
+    foreach ($csvData as $data){
+
+        if(!is_array($data)){
+            continue;
+        }
+
+        if($isHeader){
+            $isHeader=false;
+            continue;   
+        }
+
+        if ($maker != $data[$keyMaker]){
+            $maker=$data[$keyMaker];
+            $makers[]=$maker;
+        }
+        /*if($model!= $data[$keyModel]){
+            $model=$data[$keyModel];
+            $result[$maker][]=$model;
+        }*/
+    }     
+    return $makers;
+}
+    
+
 ?>
--- index.php
+++ index.php
@@ -9,43 +9,8 @@ if(empty($csvData)){
     return false;
 }
 
-
-$header = $csvData[0];
-
-$keyMaker = array_search('make', $header);
-$keyModel = array_search('model',$header);
-
-$result=[];
-
-
-$maker = '';
-$model = '';
-$isHeader=true;
-foreach ($csvData as $data){
-
-    if(!is_array($data)){
-        continue;
-    }
-
-    if($isHeader){
-        $isHeader=false;
-        continue;   
-    }
-
-    if ($maker != $data[$keyMaker]){
-        $maker=$data[$keyMaker];
-        print("$maker ");
-    }
-        
-    if($model!= $data[$keyModel]){
-        $model=$data[$keyModel];
-        $result[$maker][]=$model;
-    }
-}
-
-//print_r($result);
-
-
+$makers=getmakers($csvData);
+print_r($makers);
 
 
 ?>      
