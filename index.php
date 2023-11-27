09b2494216dcf5cf25437841581719e55a3d4bdc
3041TAN-03 <3041TAN-03@boronkay.local>
2023-11-27 12:05:31 +0100

    gyarto beszurasa az adatbazisba

--- index.php
+++ index.php
@@ -9,8 +9,35 @@ if(empty($csvData)){
     return false;
 }
 
-$makers=getmakers($csvData);
-print_r($makers);
 
+$makers=getmakers($csvData);
+
+$mysqli = new mysqli("localhost","root",null,"cars");
+
+// Check connection
+if ($mysqli -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    exit();
+}
+
+echo "connected <br>";
+
+$mysqli->query("TRUNCATE TABLE makers;");
+
+foreach ($makers as $maker){
+    $mysqli -> query("INSERT INTO makers (name) VALUES ('$maker');");
+    echo "$maker   <br>";
+}
+
+$result = $mysqli->query("SELECT COUNT('id') as cnt FROM makers;");
+$row = $result->fetch_assoc();
+echo "{$row['cnt']} sor van;\n";
+
+/*for ($x = 0; $x <= count($makers)-1; $x+=1){
+ $mysqli -> query("INSERT INTO makers (name) VALUES ('$makers[$x]');");
+    
+
+}*/
+  $mysqli -> close();
 
 ?>      
