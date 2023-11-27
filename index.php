98d380c9b64fb5de9cafb0b013cb4e9b4a486cd9
3041TAN-03 <3041TAN-03@boronkay.local>
2023-11-27 12:19:10 +0100

    rendezes

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require_once('csv-tools.php');
+require_once('db.tools.php');
 ini_set('memory_limit','1024M');
 $fileName = 'car-db.csv';
 $csvData = getCsvData($fileName);
@@ -9,35 +10,18 @@ if(empty($csvData)){
     return false;
 }
 
+$mysqli = new mysqli("localhost","root",null,"cars");
+    // Check connection
+    if ($mysqli -> connect_errno) {
+        echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+        exit();
+    }
+echo "connected <br>";
 
 $makers=getmakers($csvData);
 
-$mysqli = new mysqli("localhost","root",null,"cars");
+$result=insertMakers($mysqli,$makers, true);
 
-// Check connection
-if ($mysqli -> connect_errno) {
-    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-    exit();
-}
-
-echo "connected <br>";
-
-$mysqli->query("TRUNCATE TABLE makers;");
-
-foreach ($makers as $maker){
-    $mysqli -> query("INSERT INTO makers (name) VALUES ('$maker');");
-    echo "$maker   <br>";
-}
-
-$result = $mysqli->query("SELECT COUNT('id') as cnt FROM makers;");
-$row = $result->fetch_assoc();
-echo "{$row['cnt']} sor van;\n";
-
-/*for ($x = 0; $x <= count($makers)-1; $x+=1){
- $mysqli -> query("INSERT INTO makers (name) VALUES ('$makers[$x]');");
-    
-
-}*/
-  $mysqli -> close();
+$mysqli -> close();
 
 ?>      
