224bbf86af7dd5a650de8c3617991027e5ecaed2
3041TAN-03 <3041TAN-03@boronkay.local>
2023-12-04 09:20:15 +0100

    A CRUD fügvények elkészítése

--- db-tools.php
+++ db-tools.php
@@ -15,11 +15,39 @@ function insertMakers($mysql, $makers, $truncate = false){
     }
     return $result;
     
-    
-    /*for ($x = 0; $x <= count($makers)-1; $x+=1){
-     $mysqli -> query("INSERT INTO makers (name) VALUES ('$makers[$x]');");
-    }*/
+function updateMaker($mysqli, $data){
+    $makerName=$data['name']
+    $result = $mysql -> query("UPDATE makers SET name=$makerName");
 
+
+    if(!$result){
+        echo "hiba történt a $makerName beszúrása közben";
+        return $result;
+    }
     
+    $maker = getMakerByName($mysqli, $makerName);
+
+    return $makerName;
 }
+
+function getMaker($mysqli, $id){
+    $result = $mysqli -> query("SELECT * FROM makers WHERE id=$id");
+    $maker = $result->fetch_assoc();
+
+    return $maker;
+}
+
+function getMakerByName($mysqli, $name){
+    $result = $mysqli -> query("SELECT * FROM makers WHERE name=$name");
+    $maker = $result ->fetch_assoc();
+
+    return $maker;
+}
+
+function delMaker($mysqli, $id){
+    $result=$mysqli->query("DELETE makers WHERE id=$id");
+    
+    return $result;
+}
+
 ?>
